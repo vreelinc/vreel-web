9051d68e01bfea14e98927218a991cff2ec70451
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2022-06-23 00:01:31 +0600

    updated gallerySlider

--- src/components/Sections/Sliders/GallerySlider/GallerySlider.tsx
+++ src/components/Sections/Sliders/GallerySlider/GallerySlider.tsx
@@ -147,7 +147,7 @@ const GallerySlider: React.FC<{
                   )}
                 </div>
 
-                <div className={Styles.vreelSlide__content}>
+                <div className={clsx(Styles.galleryContainer)}>
                   <div className={Styles.vreelSlide__content_wrapper}>
                     {/* LEFT SIDEBAR */}
                     <div className={Styles.vreelSlide__content_wrapper__left}>
