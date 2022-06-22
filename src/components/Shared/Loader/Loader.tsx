e8c3501779698762e90e125ba895a94c8b5ddbb7
alasim <alasim.mail@gmail.com>
2022-06-22 07:21:09 +0600

    conflict solved

--- src/components/Shared/Loader/Loader.tsx
+++ src/components/Shared/Loader/Loader.tsx
@@ -1,12 +1,20 @@
 import clsx from "clsx";
 import React from "react";
-import { Audio } from "react-loader-spinner";
+import {
+  Audio,
+  BallTriangle,
+  Circles,
+  Oval,
+  Rings,
+  TailSpin,
+  ThreeDots,
+} from "react-loader-spinner";
 import Styles from "./Loader.module.scss";
 
 export const Loader = () => {
   return (
     <div className={Styles.loaderContainer}>
-      <Audio height="100" width="100" color="grey" ariaLabel="loading" />
+      <Circles color="#00BFFF" height={80} width={80} />
     </div>
   );
 };
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,7 @@
 import Events from "@sections/Events/Events";
 import VLinks from "@sections/VLinks/VLinks";
+import { Loader } from "@shared/Loader/Loader";
 import { Audio } from "react-loader-spinner";
 export default function test() {
-  return <Audio height="100" width="100" color="grey" ariaLabel="loading" />;
+  return <Loader />;
 }
