716a1c5c6b9dafa66845100c90f05c91a590ac50
alasim <alasim.mail@gmail.com>
2022-06-26 09:47:51 +0600

    social fixed

--- src/components/Shared/Loader/Loader.tsx
+++ src/components/Shared/Loader/Loader.tsx
@@ -14,6 +14,7 @@ import Styles from "./Loader.module.scss";
 export const Loader = () => {
   return (
     <div className={Styles.loaderContainer}>
+      {/* <div style={{ width: "100vw", height: "100vh", background: "red" }}></div> */}
       <Rings color="rgb(0,0,0)" height={80} width={80} />
     </div>
   );
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,284 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json(data);
+};
+
+export const data = {
+  data: {
+    username: {
+      id: "cafb0oq23akj9g4qk19g",
+      email: "acollins@avaicg.com",
+      username: "avangardinnovative",
+      companyName: "",
+      title: "",
+      profilePicture: "",
+      files: {
+        file_count: 21,
+      },
+      vreel: {
+        author: "cafb0oq23akj9g4qk19g",
+        logo_uri:
+          "https://staging.vreel.page/files/a4cf61c77e10cdf627b32777833d36c9",
+        elements: {
+          socials: {
+            header: "",
+            socials: [
+              {
+                platform: "instagram",
+                username: "avangard_innovative",
+              },
+              {
+                platform: "twitter",
+                username: "ai_houston_01",
+              },
+            ],
+          },
+          simple_links: {
+            header: "",
+            links: [
+              {
+                id: "caok8n223akitfbobl6g",
+                thumbnail:
+                  "https://staging.vreel.page/files/7c104d5557b0716d8d64efb053822d66",
+                link_header: "Honeywell Partners with Avangard Innovative",
+                url: "https://www.packworld.com/supplier-news/news/22018698/honeywell-performance-materials-and-technologies-pmt-honeywell-avangard-innovative-announce-joint-venture-to-build-recycling-plant",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caokbma23akitfbobl70",
+                thumbnail:
+                  "https://staging.vreel.page/files/6dd641d2d06b12ae809065ce7d0ea372",
+                link_header:
+                  "Avangard Innovative Partners with Hi-Cone Worldwide",
+                url: "https://hi-cone.com/2020/08/hi-cone-worldwide-partners-with-avangard-innovative-to-stem-plastic-waste/",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caokepa23akitfbobl7g",
+                thumbnail:
+                  "https://staging.vreel.page/files/006fc45ff8172752a1906a6080beecfd",
+                link_header: "Working to scale post-consumer resin recycling",
+                url: "https://www.greenbiz.com/video/how-avangard-innovative-working-scale-post-consumer-resin-recycling",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caoki6q23akitfbobl80",
+                thumbnail:
+                  "https://staging.vreel.page/files/97cca01317fc4a9879274af26473bfa9",
+                link_header:
+                  " Avangard Innovative: Named a 2020 Top Foam Technology Solution Provider",
+                url: "https://www.themanufacturingoutlook.com/vendor/avangard-innovative-ai-delivering-technologydriven-circular-economy-solutions--cid-52-mid-7.html",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caokm8223akitfbobl8g",
+                thumbnail:
+                  "https://staging.vreel.page/files/7604bc39f16c750cca76e065c80df5fb",
+                link_header:
+                  "U.S. Recycling Industry Is Struggling To Figure Out A Future Without China",
+                url: "https://www.npr.org/2019/08/20/750864036/u-s-recycling-industry-is-struggling-to-figure-out-a-future-without-china?fbclid=IwAR1sc-q4h32xQCK3oU4i7bfai-0r7F3zNbC8d9Nx6FByLM0NYeJ2_ECSwGw",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caokot223akitfbobl90",
+                thumbnail:
+                  "https://staging.vreel.page/files/278fc456f67f1f772b882e18c21d99f1",
+                link_header:
+                  "Avangard Innovative Transforms Recycling with Tibco",
+                url: "https://www.tibco.com/customers/success-stories",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caokq6a23akitfbobl9g",
+                thumbnail:
+                  "https://staging.vreel.page/files/f8067e7156d0fd61c9fa4f46ddcb66d7",
+                link_header:
+                  "Avangard to build three $30 million film facilities next year",
+                url: "https://resource-recycling.com/plastics/2019/09/11/avangard-to-build-three-30-million-film-facilities-next-year/",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caokrri23akitfbobla0",
+                thumbnail:
+                  "https://staging.vreel.page/files/7eba166367b2539a9001fe9e55857859",
+                link_header:
+                  "States Look for New Ways to Recycle Your Plastic and Paper",
+                url: "https://www.wsj.com/articles/states-look-for-new-ways-to-recycle-your-plastic-and-paper-11567935001?emailToken=3f8f98647f20f5ee54786d7ad63510a5k8x497koADzz3BVXo553+/f+HbAiUKlg11AUf54HSxQttVdhucT5EuiN/NfjwaqZYXe2IcKgfrVRYdz+IdppDuU6vVbTdw0OsqZxdF9wTD8RN9wCYkvdxtlB2krTwLEP&reflink=article_gmail_share",
+                link_type: "url",
+                tag: "news",
+              },
+              {
+                id: "caoktrq23akitfboblag",
+                thumbnail:
+                  "https://staging.vreel.page/files/6c52a43dfea4e7fac53c3de823242c18",
+                link_header: "Natura Zero 2.0",
+                url: "https://www.avaicg.com/naturazero20",
+                link_type: "url",
+                tag: "services",
+              },
+              {
+                id: "caokv5q23akitfboblb0",
+                thumbnail:
+                  "https://staging.vreel.page/files/1b64f16329156ec96d7666b1c262d825",
+                link_header: "Sustayn Analytics",
+                url: "https://www.avaicg.com/sustayn",
+                link_type: "url",
+                tag: "services",
+              },
+              {
+                id: "caokvuq23akitfboblbg",
+                thumbnail:
+                  "https://staging.vreel.page/files/25705b6ee772134fc4ff2ad8caded656",
+                link_header: "Natura PCR",
+                url: "https://www.avaicg.com/naturapcr",
+                link_type: "url",
+                tag: "services",
+              },
+              {
+                id: "caol1bi23akitfboblc0",
+                thumbnail:
+                  "https://staging.vreel.page/files/9769eb328d3d596c1aefb53f67b38044",
+                link_header: "Organics",
+                url: "https://www.avaicg.com/organics",
+                link_type: "url",
+                tag: "services",
+              },
+              {
+                id: "caol24223akitfboblcg",
+                thumbnail:
+                  "https://staging.vreel.page/files/793f6a1f521cb61b5fb74f633b137846",
+                link_header: "Equipment",
+                url: "https://www.avaicg.com/equipment",
+                link_type: "url",
+                tag: "services",
+              },
+            ],
+          },
+        },
+        slides: [
+          {
+            id: "caqvlga23akqutk0agc0",
+            slide_location: 1,
+            content_type: "",
+            uri: "",
+            title: {
+              header: "Rick Perez, Founder & CEO, Avangard Innovative",
+              description: "The Sustainability and Circular Economy",
+            },
+            advanced: {
+              header: "",
+            },
+            mobile: {
+              start_time: null,
+              stop_time: null,
+              background_audio_uri: "",
+              uri: "https://res.cloudinary.com/klwebco/video/upload/v1656170341/samples/Slide_2_-_Rick_Perez_Founder_CEO_Avangard_Innovative_-_The_Sustainability_and_Circular_Economy_-_Mobile_tdpcfd.mp4",
+              content_type: "video/mp4",
+            },
+            desktop: {
+              start_time: null,
+              stop_time: null,
+              background_audio_uri: "",
+              uri: "https://res.cloudinary.com/klwebco/video/upload/v1656170401/samples/Slide_2_-_Rick_Perez_Founder_CEO_Avangard_Innovative_-_The_Sustainability_and_Circular_Economy-_Desktop_idjsqr.mp4",
+              content_type: "video/mp4",
+            },
+            cta1: {
+              link_header: "",
+              link_type: "",
+              link_url: "",
+            },
+            cta2: {
+              link_header: "",
+              link_type: "",
+              link_url: "",
+            },
+          },
+          {
+            id: "cajag5223aktcaat7b20",
+            slide_location: 1,
+            content_type: "",
+            uri: "",
+            title: {
+              header: "Avangard Innovative",
+              description: "Think Circular",
+            },
+            advanced: {
+              header: "",
+            },
+            mobile: {
+              start_time: null,
+              stop_time: null,
+              background_audio_uri: "",
+              uri: "https://res.cloudinary.com/klwebco/video/upload/v1655863954/samples/aiexplainer_optimized_o24q3q.mp4",
+              content_type: "video/mp4",
+            },
+            desktop: {
+              start_time: null,
+              stop_time: null,
+              background_audio_uri: "",
+              uri: "https://res.cloudinary.com/klwebco/video/upload/v1655863954/samples/aiexplainer_optimized_o24q3q.mp4",
+              content_type: "video/mp4",
+            },
+            cta1: {
+              link_header: "",
+              link_type: "",
+              link_url: "",
+            },
+            cta2: {
+              link_header: "",
+              link_type: "",
+              link_url: "",
+            },
+          },
+          {
+            id: "caqv4hi23akh0p8pgkng",
+            slide_location: 2,
+            content_type: "",
+            uri: "",
+            title: {
+              header: "Dow",
+              description:
+                "Julie Zaniewski, Sustainability Director, Interviewed by GreenBiz",
+            },
+            advanced: {
+              header: "",
+            },
+            mobile: {
+              start_time: null,
+              stop_time: null,
+              background_audio_uri: "",
+              uri: "https://res.cloudinary.com/klwebco/video/upload/v1656170341/samples/Slide_2_-_Rick_Perez_Founder_CEO_Avangard_Innovative_-_The_Sustainability_and_Circular_Economy_-_Mobile_tdpcfd.mp4",
+              content_type: "video/mp4",
+            },
+            desktop: {
+              start_time: null,
+              stop_time: null,
+              background_audio_uri: "",
+              uri: "",
+              content_type: "video/mp4",
+            },
+            cta1: {
+              link_header: "",
+              link_type: "",
+              link_url: "",
+            },
+            cta2: {
+              link_header: "",
+              link_type: "",
+              link_url: "",
+            },
+          },
+        ],
+      },
+    },
+  },
+};
