654cb55c2bcfd138102b1737c9939005e1ffa2b2
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2022-06-27 12:40:43 +0600

    testing video

--- src/pages/test3.tsx
+++ src/pages/test3.tsx
@@ -7,7 +7,19 @@ import React from "react";
 function test3() {
   return (
     <MainContainer>
-      <VLinks />
+      <video
+        style={{ position: "absolute", zIndex: "999", top: "0", left: "0" }}
+        autoPlay
+        loop
+        muted
+        width="100%"
+        height="400"
+      >
+        <source
+          src="https://www.youtube.com/embed/29avsYeMkdc"
+          type="video/mp4"
+        />
+      </video>
     </MainContainer>
   );
 }
