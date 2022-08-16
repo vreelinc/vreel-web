3d8018ee021093b114e8d5837584a2f72e3fcd04
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2022-08-16 11:51:47 +0600

    local changes

--- src/pages/test4.tsx
+++ src/pages/test4.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import Test4 from 'src/components/Test/Test4/Test4';
-import Test5 from 'src/components/Test/Test5/Test5';
+import React, { useRef, useState } from "react";
+import ReactHlsPlayer from "react-hls-player";
 
 export default function test4() {
+  const [hlsUrl, setHlsUrl] = useState(
+    "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+  );
+  const videoRef = useRef();
   return (
     <div>
-      <Test4 />
-      {/* <Test5 /> */}
+      {/* <ReactHlsPlayer
+        playerRef={videoRef}
+        src={hlsUrl}
+        autoPlay={false}
+        controls={true}
+        width="60%"
+        height="auto"
+        hlsConfig={{
+          maxLoadingDelay: 4,
+          minAutoBitrate: 0,
+          lowLatencyMode: true,
+          enableWorker: true,
+        }}
+      /> */}
     </div>
   );
 }
