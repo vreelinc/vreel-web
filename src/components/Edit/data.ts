49cd716177afcc160f2d4d850aed254a03e6d25e
Aaron Marsh <aaronmarsh755@gmail.com>
2022-09-12 23:55:31 -0400

    fixed elements => sections

--- src/components/Edit/data.ts
+++ src/components/Edit/data.ts
@@ -25,7 +25,7 @@ export const regularOptions: Array<EditItem> = [
     children: [
       { title: 'File Manager', href: '/edit/edit_vreel/files' },
       { title: 'Slides', href: '/edit/edit_vreel/slides' },
-      { title: 'Elements', href: '/edit/edit_vreel/elements' },
+      { title: 'Sections', href: '/edit/edit_vreel/elements' },
       {
         title: 'Display Options',
         href: '/edit/edit_vreel/display_options',
