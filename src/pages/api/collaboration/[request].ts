b55541c4db90df82b7802675f0ae4f6219f58036
Aaron Marsh <aaronmarsh755@gmail.com>
2022-12-12 02:11:24 -0500

    fixed non redirect

--- src/pages/api/collaboration/[request].ts
+++ src/pages/api/collaboration/[request].ts
@@ -6,18 +6,20 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const token = req.cookies.userAuthToken;
     const requestId = req.query.request;
     if (!requestId) return res.status(401).json({ err: "missing request id" })
-    if (!token) return res.writeHead(301, '/login');
+    if (!token) return res.redirect(307, 'https://vreel.page');
     client.mutate({
         mutation: ACCEPT_COLLABORATION_REQUEST,
         variables: {
             token: token,
             requestId,
         }
-    }).then(console.log)
+    })
         .finally(() => {
-            res.writeHead(301, '/')
+            console.log("finished !!")
+            res.redirect(307, 'https://vreel.page')
+            // res.end()
         })
-        .catch(console.log)
-    res.status(200).json({ name: req.query.request, cookie: req.cookies })
+
+    // res.status(200).json({ name: req.query.request, cookie: req.cookies })
 }
 
